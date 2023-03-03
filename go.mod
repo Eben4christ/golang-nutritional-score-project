16c615c6885adfed5dc99a5c142112864f2df8c3
Ebenezer <Oluwatadeebenezer@gmail.com>
2023-03-03 06:59:46 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-nutritional-score-project
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ns := GetNutritionalScore(NutritionalData{
+		Energy: EnergyFromKcal(0),
+		Sugars: SugarGram(10),
+		SaturatedFattyAcids: SaturatedFattyAcids(2),
+		Sodium: SodiumMilligram(500),
+		Fruits: FruitsPercent(60),
+		Fibre: FibreGram(4),
+		Protein: ProteinGram(2),
+	}, Food)
+
+	fmt.Printf("Nutritional Score:%d\n", ns.Value)
+	fmt.Printf("NutriScore: %s\n", ns.GetNutriscore())
+}
